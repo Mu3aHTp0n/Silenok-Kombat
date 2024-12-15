edc9c90f48a63b6cd084b14c0822b2044c45eaf2
Mu3aHTp0n <kovalevartur05@gmail.com>
2024-12-15 11:58:09 +0300

    feature: error message if email field is empty

--- app/components/FormField.tsx
+++ app/components/FormField.tsx
@@ -1,6 +1,7 @@
 import {TextInput, View, Text, Image, TouchableOpacity} from "react-native";
 import React, {useState} from 'react';
 import icons from "../../constants/icons";
+import axios from "axios";
 
 interface IComponentProps {
 	title: string,
@@ -13,12 +14,31 @@ interface IComponentProps {
 function FormField({title, value, placeholder, handleChangeText, otherStyles}: IComponentProps) {
 	const [showPassword, setShowPassword] = useState(false)
 	const [isFocused, setIsFocused] = useState(false);
+	const [isError, setIsError] = useState(false);
+
+	function codeRequest() {
+		if (value !== '') {
+			setIsError(false);
+			axios.post('demo/sendCode', {
+				email: value
+			})
+				.then(response => {
+					console.log(response)
+				})
+				.catch(error => {
+					console.log(error)
+				})
+		} else {
+			setIsError(true);
+		}
+	}
 
 	return (
 		<View className={`space-y-2 ${otherStyles}`}>
 			<Text className='text-gray-400 text-xl'>{title}</Text>
-			<View className={`rounded-2xl h-16 w-full bg-purple-900 px-4 border-2 justify-center items-center flex-row border-fuchsia-950 ${isFocused ? 'border-white' : 'border-fuchsia-950'}`}>
-				<TextInput className='flex-1 text-white text-base' value={value}
+			<View className={`rounded-2xl h-16 w-full bg-purple-900 px-4 border-2 justify-center items-center flex-row border-fuchsia-950 ${isError ? 'border-red-600' : ''} ${isFocused ? 'border-white' : 'border-fuchsia-950'}`}>
+				<TextInput className='flex-1 text-white text-base'
+						   value={value}
 						   placeholder={placeholder}
 						   placeholderTextColor="#7b7b8b"
 						   onChangeText={handleChangeText}
@@ -29,20 +49,21 @@ function FormField({title, value, placeholder, handleChangeText, otherStyles}: I
 						setShowPassword(!showPassword);
 					}}>
 						<Image source={!showPassword ? icons.eye : icons.closedEye}
-							   className='w-6 h-6'
+							   className='w-8 h-8'
 							   resizeMode='contain'/>
 					</TouchableOpacity>
 				)}
 				{title === 'Электронная почта' && (
 					<TouchableOpacity onPress={() => {
-						null
+						codeRequest()
 					}}>
 						<Image source={icons.email}
-							   className='w-8 h-8'
+							   className='w-10 h-10'
 							   resizeMode='contain'/>
 					</TouchableOpacity>
 				)}
 			</View>
+			{isError && <Text className="text-red-600">Заполните поле</Text>}
 		</View>
 	);
 }
